ed7324adfcfa9e885f8b4a43d7269ca95c7ce0b8
Michael Allan <michaelallan@optushome.com.au>
2013-05-05 23:50:37 +1000

    test suite working with requirejs

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,11 +1,32 @@
-/*global describe, it */
+/*global define, describe, it, expect */
 'use strict';
-(function () {
-    describe('Give it some context', function () {
-        describe('maybe a bit more context here', function () {
-            it('should run here few assertions', function () {
 
-            });
+describe('chai lib sanity check', function () {
+    it('should work as per the api docs', function () {
+
+        expect(true).to.equal(true);
+        expect(true).to.not.equal(false);
+        expect('foo'.length).to.equal(3);
+
+    });
+});
+
+define(['dom', 'game'], function (dom, game) {
+    describe('dom module tests', function () {
+        it('should have meet some initial criteria', function () {
+
+            expect(dom).to.have.property('hasClass');
+            expect(dom).to.not.have.property('pollyWaffle');
+
+        });
+    });
+
+    describe('game module tests', function () {
+        it('should have meet some initial criteria', function () {
+
+            expect(game).to.be.an('object');
+            expect(game).to.include.keys('showScreen');
+
         });
     });
 })();
